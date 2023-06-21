3c20b14c7c7ab33308408a2f0bb146d2db6eddbd
최유정 <133944035+cheezcyj@users.noreply.github.com>
2023-06-21 17:36:32 +0900

    Controller수정

--- src/main/java/com/pcwk/ehr/nmemeber/NotMemberController.java
+++ src/main/java/com/pcwk/ehr/nmemeber/NotMemberController.java
@@ -1,34 +1,23 @@
 package com.pcwk.ehr.nmemeber;
 
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pcwk.ehr.resultUrl.Result_UrlVO;
 
 @Controller
 public class NotMemberController {
 
-    private final NotMemberService notMemberService;
+	@Autowired
+	NotMemberService NotMemberService;
 
-    @Autowired
-    public NotMemberController(NotMemberService notMemberService) {
-        this.notMemberService = notMemberService;
-    }
-
-    @GetMapping("/")
-    public String showForm(Model model) {
-        model.addAttribute("notMemberVO", new NotMemberVO());
-        return "input-form";
-    }
-
-    @PostMapping("/submit")
-    public String submitForm(@ModelAttribute("notMemberVO") NotMemberVO notMemberVO, Model model) {
-        notMemberService.saveUserInfo(notMemberVO.getMbti(), notMemberVO.getNickname(), notMemberVO.getGender(), notMemberVO.getAni());
-
-        model.addAttribute("notMemberVO", notMemberVO);
-
-        return "result-page";
-    }
+	@RequestMapping(value = "/ELCARO/NotMember.do")
+	public String resultSelect(Result_UrlVO search, Model model) throws SQLException{
+		
+		return "user/Ne03_NotMember";
+	}
 }
